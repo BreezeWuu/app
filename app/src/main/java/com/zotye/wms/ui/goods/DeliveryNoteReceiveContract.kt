38fac6204c27521ca8bd05633f9f08a542bc3fdc
hechuangju <chuangjuhe@gmail.com>
2018-03-07 12:22:46 +0800

    *):下架确认

---
+++ app/src/main/java/com/zotye/wms/ui/goods/DeliveryNoteReceiveContract.kt
@@ -0,0 +1,58 @@
+package com.zotye.wms.ui.goods
+
+import com.zotye.wms.R
+import com.zotye.wms.data.AppExecutors
+import com.zotye.wms.data.DataManager
+import com.zotye.wms.data.api.ApiResponse
+import com.zotye.wms.data.api.model.receipt.DeliveryNoteInfoResponse
+import com.zotye.wms.ui.common.BasePresenter
+import com.zotye.wms.ui.common.MvpPresenter
+import com.zotye.wms.ui.common.MvpView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
+
+/**
+ * Created by hechuangju on 2018/03/07
+ */
+object DeliveryNoteReceiveContract {
+
+    interface DeliveryNoteReceiveView : MvpView {
+        fun getDeliveryNoteInfoByCode(data: DeliveryNoteInfoResponse?)
+    }
+
+    interface DeliveryNoteReceivePresenter : MvpPresenter<DeliveryNoteReceiveView> {
+        fun getDeliveryNoteInfoByCode(barCode: String)
+    }
+
+    class DeliveryNoteReceivePresenterImpl @Inject constructor(private val dataManager: DataManager, private val appExecutors: AppExecutors) : BasePresenter<DeliveryNoteReceiveView>(), DeliveryNoteReceivePresenter {
+
+        override fun getDeliveryNoteInfoByCode(barCode: String) {
+            mvpView?.showProgressDialog(R.string.loading_query_bar_code_info)
+            appExecutors.diskIO().execute {
+                dataManager.getCurrentUser()?.let {
+                    appExecutors.mainThread().execute {
+                        dataManager.getDeliveryNoteInfoByCode(it.userId, barCode).enqueue(object : Callback<ApiResponse<DeliveryNoteInfoResponse>> {
+                            override fun onFailure(call: Call<ApiResponse<DeliveryNoteInfoResponse>>?, t: Throwable) {
+                                mvpView?.hideProgressDialog()
+                                t.message?.let { mvpView?.showMessage(it) }
+                            }
+
+                            override fun onResponse(call: Call<ApiResponse<DeliveryNoteInfoResponse>>?, response: Response<ApiResponse<DeliveryNoteInfoResponse>>) {
+                                mvpView?.hideProgressDialog()
+                                response.body()?.let {
+                                    if (it.isSucceed()) {
+                                        mvpView?.getDeliveryNoteInfoByCode(it.data)
+                                    } else {
+                                        mvpView?.showMessage(it.message)
+                                    }
+                                }
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/zotye/wms/ui/goods/DeliveryNoteReceiveFragment.kt
@@ -0,0 +1,16 @@
+package com.zotye.wms.ui.goods
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zotye.wms.ui.common.BaseFragment
+
+/**
+ * Created by hechuangju on 2018/03/07
+ */
+class DeliveryNoteReceiveFragment : BaseFragment() {
+    override fun onCreateContentView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): View? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
